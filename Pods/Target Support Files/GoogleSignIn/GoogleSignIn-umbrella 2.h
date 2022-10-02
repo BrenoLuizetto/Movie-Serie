2e9ad236d15f615c5aa3056b1059e4b37feb9064
BrenoLuizetto <cintrabreno22@gmail.com>
2022-10-02 19:31:08 -0300

    update changes

---
+++ Pods/Target Support Files/GoogleSignIn/GoogleSignIn-umbrella 2.h
@@ -0,0 +1,30 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "GIDAuthentication 3.h"
+#import "GIDAuthentication.h"
+#import "GIDConfiguration 3.h"
+#import "GIDConfiguration.h"
+#import "GIDGoogleUser 3.h"
+#import "GIDGoogleUser.h"
+#import "GIDProfileData 3.h"
+#import "GIDProfileData.h"
+#import "GIDSignIn 3.h"
+#import "GIDSignIn.h"
+#import "GIDSignInButton 3.h"
+#import "GIDSignInButton.h"
+#import "GoogleSignIn 3.h"
+#import "GoogleSignIn.h"
+
+FOUNDATION_EXPORT double GoogleSignInVersionNumber;
+FOUNDATION_EXPORT const unsigned char GoogleSignInVersionString[];
+
